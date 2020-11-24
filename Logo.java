6ea765b5e8aa81d69155b7c98e3cbeded09ad97a
AeonHUB <49218225+AeonHUB@users.noreply.github.com>
2020-11-24 13:30:23 -0800

    added next and back buttons

--- Logo.java
+++ Logo.java
@@ -1,11 +1,15 @@
+package Logonew;
+
 // needed for doing graphics
 import java.awt.*;
 import java.awt.geom.*;
 import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 /**
-* @author -Alex Rogers
-* 
+ * @author -Alex Rogers
+ * 
  * --------------------------
  */
 
@@ -23,60 +27,60 @@ public class Logo extends JPanel
     int q2y = 400;
     int q3x = q0x;
     int q3y = q2y;
-    
+
     int currentState = 0;
     int currentFrame = 0;
-    
-    
+
+            
+            
+    //b.setBounds(50,50,90,50);
+    //frame.add(b);
 
     public static void main(String[] args)
     {
         //Create a window frame
         JFrame myFrame = new JFrame();
+        JButton BackButton = new JButton("BACK");
+        JButton NextButton = new JButton("NEXT");
+        myFrame.add(BackButton);
+        myFrame.add(NextButton);
+        BackButton.setBounds(25,25,80, 40);
+        NextButton.setBounds(25,80,80, 40);
         myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         myFrame.setSize(900,700);
 
         // put the picture in the frame
-        
+
         Logo logo = new Logo();
         logo.init();
         myFrame.add(logo);
-        
-        
-        
-        
-        
+
         myFrame.setVisible(true);
         //System.out.println("hello again");
-        
-        
-        
-    }
 
+    }
     public void init() 
     {      
         String question = "What string would you like to test? (Enter 1, 2, or 3)";
         String numString = JOptionPane.showInputDialog(question);
         testString = Integer.parseInt(numString);        
     }
-    
+
     public void paint(Graphics g) { 
         super.paint(g);
         initPaint(g);
-       
-        
-        
+
         int x = 0; 
         //TODO: while loop to change jframe
         while(x > 3){
-            
-            
+
         }
     }
+
     public void initPaint(Graphics g){
         drawState(g, q1x+75, q0y-75,4); //q1 arrow to itself, 4 removes it from accept color possiblity
         drawState(g, q2x+75, q2y+75,4);
-        
+
         drawState(g, q0x, q0y,0); //q0 
         drawState(g, q1x, q0y,1); //q1
         drawState(g, q2x, q2y,2);//q2
@@ -87,20 +91,19 @@ public class Logo extends JPanel
         g.setColor(Color.WHITE);
         g.fillOval(q3x+15,q3y+15,120,120);  
         g.setColor(Color.BLACK);
-        
+
         g.fillRect(150+q0x,70+q0y,q1x-145-q0x,10);
         g.fillRect(q1x+70,150+q0y,10,q2y-145-q1y);
         g.fillRect(150+q0x,70+q3y,q1x-145-q0x,10);
         g.fillPolygon(new int[] {q1x+5, q1x-10,q1x-10}, new int[] {75+q0y,90+q0y,60+q0y}, 3);//first arrow
         g.fillPolygon(new int[] {q2x+75, q1x+90,q1x+60}, new int[] {q2y+5,q2y-10,q2y-10}, 3);//seccond arrowhead
         g.fillPolygon(new int[] {q3x+145, q3x+160,q3x+160}, new int[] {75+q3y,95+q3y,55+q3y}, 3);//third arrowhead
-        
+
         g.fillPolygon(new int[] {q2x+75,q2x+65, q2x+85}, new int[] {145+q2y,160+q2y,160+q2y}, 3);
         g.fillPolygon(new int[] {q2x+75, q2x+65,q2x+85}, new int[] {q1y+5,q1y-10,q1y-10}, 3);
 
-    
     }
-    
+
     public void drawState(Graphics g, int x, int y, int stateNum)
     {
         // state with black outline
@@ -109,19 +112,19 @@ public class Logo extends JPanel
             g.drawOval(x+0,y+0,150,150);
         }
         else{
-        g.fillOval(x+0,y+0,150,150);
+            g.fillOval(x+0,y+0,150,150);
             if(currentState == stateNum){
-            g.setColor(Color.GREEN);
-        }
+                g.setColor(Color.GREEN);
+            }
             g.setColor(Color.WHITE);
             g.fillOval(x+5,y+5,140,140);  
-       }
+        }
     }
-    
+
     public void initText(Graphics g){
         //testString if cases
-        
+
         //directed graph labels
-        
+
     }
 }
